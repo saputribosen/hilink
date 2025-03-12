6e0a85e032c83b3dc029abea1fcdd30ffbb0db10
sarwenda <142522186+saputribosen@users.noreply.github.com>
2025-03-13 00:00:22 +0700

    Update hilink.lua

--- controller/hilink.lua
+++ controller/hilink.lua
@@ -2,5 +2,5 @@ module("luci.controller.hilink", package.seeall)
 
 function index()
     -- Menambahkan entri di Luci
-    entry({"admin", "services", "hilink"}, cbi("hilink"), _("Hilink Monitor"), 93).dependent = true
+    entry({"admin", "services", "hilink"}, cbi("hilink"), _("Hilink Monitor"), 90).dependent = true
 end
